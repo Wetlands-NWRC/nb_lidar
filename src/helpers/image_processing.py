a05812985cd20f565ee5e023b3efd09111739133
Hamilton97 <ryangilberthamilton@gmail.com>
2024-05-28 17:18:56 -0400

    refactor raster calcs: move to image processing

--- src/helpers/image_processing.py
+++ src/helpers/image_processing.py
@@ -4,6 +4,51 @@ import ee
 from src.helpers import rsd
 from src.helpers import calcs
 
+## Optical Raster Calcs
+def _compute_ndvi(image, nir, red):
+    return image.normalizedDifference([nir, red]).rename('NDVI')
+
+
+def _compute_savi(image, nir, red) -> ee.Image:
+    return image.expression(
+            "(1 + L) * (NIR - RED) / (NIR + RED + L)",
+            {"NIR": image.select(nir), "RED": image.select(red), "L": L},
+        ).rename("SAVI")
+
+
+def _compute_tasseled_cap(image, *bands) -> ee.Image:
+    if not bands:
+        bands = list(range(0,6))
+    else:
+        bands = list(bands)
+
+    if len(bands) != 6:
+        raise ValueError("Must Provide exactly 6 bands")
+
+    coefficients = ee.Array(
+        [
+            [0.3029, 0.2786, 0.4733, 0.5599, 0.508, 0.1872],
+            [-0.2941, -0.243, -0.5424, 0.7276, 0.0713, -0.1608],
+            [0.1511, 0.1973, 0.3283, 0.3407, -0.7117, -0.4559],
+            [-0.8239, 0.0849, 0.4396, -0.058, 0.2013, -0.2773],
+            [-0.3294, 0.0557, 0.1056, 0.1855, -0.4349, 0.8085],
+            [0.1079, -0.9023, 0.4119, 0.0575, -0.0259, 0.0252],
+        ]
+    )
+    array_image = image.select(bands).toArray()
+    array_image_2d = array_image.toArray(1)
+
+    components = (
+        ee.Image(coefficients)
+        .matrixMultiply(array_image_2d)
+        .arrayProject([0])
+        .arrayFlatten(
+            [["brightness", "greenness", "wetness", "fourth", "fifth", "sixth"]]
+        )
+    )
+
+    return components.select(["brightness", "greenness", "wetness"])
+
 
 def fetch_and_process_data_cube(aoi) -> ee.Image:
     """fetch and apply processing steps to Data Cube Composites"""
