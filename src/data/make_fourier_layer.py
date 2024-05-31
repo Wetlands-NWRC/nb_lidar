b9171767f30d5bf99a904dfeb6741183669474ec
Hamilton97 <ryangilberthamilton@gmail.com>
2024-05-31 11:14:02 -0400

    updates

--- src/data/make_fourier_layer.py
+++ src/data/make_fourier_layer.py
@@ -5,10 +5,9 @@ from src.helpers.compute_fouirer import compute_fourier_transform
 _add_ndvi = lambda x: x.addBands(x.normalizedDifference(["B8", "B4"]).rename("NDVI"))
 
 
-def make_fourier_transform():
+def make_fourier_transform(dataset, dependent, modes):
     # check to make sure that the destination asset dosnet conflict
     modes = 3
-
     dataset = (
         Sentinel2TOA()
         .filterBounds()
