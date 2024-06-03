e564df94353817b9ecc195ae75333a46af687cfb
Hamilton97 <ryangilberthamilton@gmail.com>
2024-06-03 10:29:36 -0400

    update exp3 for export objs to cloud storage

--- src/models/train_and_assess.py
+++ src/models/train_and_assess.py
@@ -5,7 +5,7 @@ import ee.batch
 
 
 from src.helpers.classifier import SmileRandomForest
-from src.utils.datautils import table_to_drive, monitor_task
+# from src.utils.datautils import table_to_drive, monitor_task
 
 
 def format_error_matrix_for_export(
@@ -23,59 +23,59 @@ def format_error_matrix_for_export(
     return ee.FeatureCollection(features)
 
 
-def train_smile_random_forest_and_assess(
-    features: ee.FeatureCollection,
-    n_trees: int = 1000,
-    label_col: str = "class_name",
-    remove_props: list = None,
-    split_column: str = "random",
-    split: float = 0.7,
-) -> tuple[SmileRandomForest, ee.confusionmatrix.ConfusionMatrix]:
+# def train_smile_random_forest_and_assess(
+#     features: ee.FeatureCollection,
+#     n_trees: int = 1000,
+#     label_col: str = "class_name",
+#     remove_props: list = None,
+#     split_column: str = "random",
+#     split: float = 0.7,
+# ) -> tuple[SmileRandomForest, ee.confusionmatrix.ConfusionMatrix]:
 
-    to_remove = ["system:index", "random", "eco_region", "class_name"]
-    if remove_props:
-        to_remove.extend(remove_props)
+#     to_remove = ["system:index", "random", "eco_region", "class_name"]
+#     if remove_props:
+#         to_remove.extend(remove_props)
 
-    predictors = features.first().propertyNames().removeAll(to_remove)
+#     predictors = features.first().propertyNames().removeAll(to_remove)
 
-    train = features.filter(f"{split_column} < {split}")
-    test = features.filter(f"{split_column} >= {split}")
+#     train = features.filter(f"{split_column} < {split}")
+#     test = features.filter(f"{split_column} >= {split}")
 
-    model = SmileRandomForest(ntrees=n_trees)
-    model.train(train, label_col, predictors)
+#     model = SmileRandomForest(ntrees=n_trees)
+#     model.train(train, label_col, predictors)
 
-    order = test.aggregate_array("class_name").distinct()
-    confusion_matrix = model.error_matrix(test, order=order)
+#     order = test.aggregate_array("class_name").distinct()
+#     confusion_matrix = model.error_matrix(test, order=order)
 
-    return model, confusion_matrix
-
-
-def save_model(model_name: str, model: SmileRandomForest) -> int:
-    model_task = model.save(model_name)
-
-    print(f"Exporting Model: {model_name}")
-    status_code = monitor_task(model_task)
-    if status_code > 0:
-        raise RuntimeError(
-            f"An error occurred while saving the model. Exit code: {status_code}"
-        )
-    return status_code
+#     return model, confusion_matrix
 
 
-def save_confusion_matrix_to_drive(obj, folder, name) -> None:
-    if isinstance(obj, ee.confusionmatrix.ConfusionMatrix):
-        obj = format_error_matrix_for_export(obj)
+# def save_model(model_name: str, model: SmileRandomForest) -> int:
+#     model_task = model.save(model_name)
 
-    task = ee.batch.Export.table.toDrive(
-        collection=obj,
-        description="",
-        folder=folder,
-        fileNamePrefix=name,
-        fileFormat="GeoJSON",
-    )
+#     print(f"Exporting Model: {model_name}")
+#     status_code = monitor_task(model_task)
+#     if status_code > 0:
+#         raise RuntimeError(
+#             f"An error occurred while saving the model. Exit code: {status_code}"
+#         )
+#     return status_code
 
-    task.start()
-    return None
+
+# def save_confusion_matrix_to_drive(obj, folder, name) -> None:
+#     if isinstance(obj, ee.confusionmatrix.ConfusionMatrix):
+#         obj = format_error_matrix_for_export(obj)
+
+#     task = ee.batch.Export.table.toDrive(
+#         collection=obj,
+#         description="",
+#         folder=folder,
+#         fileNamePrefix=name,
+#         fileFormat="GeoJSON",
+#     )
+
+#     task.start()
+#     return None
 
 
 def main(args: list[str]) -> int:
